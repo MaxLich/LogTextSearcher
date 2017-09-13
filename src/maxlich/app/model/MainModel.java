c0bda3049e7672985893fe42795cddb5770ad4b4
maxlich <lich-max@mail.ru>
2017-09-13 16:04:54 +0300

    make a changes in two methods of the class 'MainModel'
    * change the method 'deleteAllEmptyBranches()':
     - replace recursion with a queue
    * make a small change in the method 'addChildrenNodes()':
     - change the condition for checking folders on emptiness

--- src/maxlich/app/model/MainModel.java
+++ src/maxlich/app/model/MainModel.java
@@ -3,15 +3,14 @@ package maxlich.app.model;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeModel;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.*;
 
 public class MainModel extends Model {
     private String extensionOfFiles;
@@ -45,7 +44,8 @@ public class MainModel extends Model {
                     if (fileName.endsWith("."+extensionOfFiles) && isFileContainsText(path)) //если файл имеет нужное расширение и содержит искомый текст
                         rootNode.add(new DefaultMutableTreeNode(new PathContainer(path),false)); //то добавляет его в дерево
                 } else if (Files.isDirectory(path)) {
-                    if (path.toFile().listFiles().length == 0) continue;
+                    File[] listFiles = path.toFile().listFiles();
+                    if (listFiles == null || listFiles.length == 0) continue;
                     DefaultMutableTreeNode newChild = new DefaultMutableTreeNode(new PathContainer(path), true);
                     rootNode.add(newChild); //добавляем папку в дерево
                     addChildrenNodes(newChild,path); //рекурсивный вызов метода с данной папкой и данным узлом в качестве параметров
@@ -81,18 +81,39 @@ public class MainModel extends Model {
     //метод очищает дерево от пустых веток (содержащих одни пустые каталоги)
     private void deleteAllEmptyBranches(DefaultMutableTreeNode currNode) {
         if (currNode == null) return;
+        Enumeration<DefaultMutableTreeNode> children = currNode.children();
+        if (children == null || !children.hasMoreElements()) return;
 
-        if (currNode.getAllowsChildren()) {
-            if (currNode.isLeaf()) {
-                DefaultMutableTreeNode parent = (DefaultMutableTreeNode) currNode.getParent();
-                currNode.removeFromParent();
-                deleteAllEmptyBranches(parent);
-            } else {
-                Enumeration children = currNode.children();
-                while (children.hasMoreElements())
-                    deleteAllEmptyBranches((DefaultMutableTreeNode)children.nextElement());
+        Queue<DefaultMutableTreeNode> queue = new LinkedList<>();
+
+        while (children.hasMoreElements()) {
+            DefaultMutableTreeNode child = children.nextElement();
+            if (child.getAllowsChildren())    //если это каталог, то добавляем его в очередь
+                queue.add(child);
+        }
+
+        while (!queue.isEmpty()) {
+            DefaultMutableTreeNode node = queue.poll();
+            if (node == null)  continue;
+            if (node.getAllowsChildren()) {
+                if (node.isLeaf()) {
+                    DefaultMutableTreeNode parent = (DefaultMutableTreeNode) node.getParent();
+                    node.removeFromParent();
+                    if (queue.contains(parent) || parent == null) continue; // если родитель этого узла уже есть в очереди
+                                                                            // или он равен нулл, то ничего не делаем
+                    queue.offer(parent); //иначе добавляем его в очередь
+                } else {
+                    children = node.children();
+                    while (children.hasMoreElements()) {
+                        DefaultMutableTreeNode child = children.nextElement();
+                        if (child.getAllowsChildren() && !queue.contains(child))    // если это каталог и его ещё нет в очереди, то...
+                            queue.add(child);                         // добавляем его в очередь
+                    }
+                }
             }
         }
+
+
     }
 
     //метод загружает в список строки из файла и передаёт этот список вьюхам для отображения
